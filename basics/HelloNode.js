c7a4a4bcdc407433011c372f7583a5f8a7f23b79
tsaarikivi <tero.saarikivi@outlook.com>
2016-10-03 13:04:03 +0300

    initial commit

---
+++ basics/HelloNode.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const http = require('http');
+
+http
+    .createServer((req, res) =>{
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end('<h1>Hello NodeJS</h1>');
+    })
+    .listen(3000, () => console.log('Server running on port 3000'));
